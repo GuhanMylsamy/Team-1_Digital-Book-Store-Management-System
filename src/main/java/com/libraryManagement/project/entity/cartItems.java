080ce0ddb09da32ac47b3251ef8fd983dcecae7e
Anand <2440589@cognizant.com>
2025-09-10 14:42:58 +0530

    added cartItems Entity

---
+++ src/main/java/com/libraryManagement/project/entity/cartItems.java
@@ -0,0 +1,70 @@
+package com.libraryManagement.project.entity;
+
+import io.micrometer.core.annotation.Counted;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cartItems")
+public class cartItems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cartItemId")
+    private String cartItemId;
+
+    //TODO: ManyToOne
+    @JoinColumn(name = "carts")
+    private String cartId;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    //TODO: ManyToOne
+    @JoinColumn(name = "bookId")
+    private String bookId;
+
+    public cartItems() {
+    }
+
+    public cartItems(String cartItemId, String cartId, int quantity, String bookId) {
+        this.cartItemId = cartItemId;
+        this.cartId = cartId;
+        this.quantity = quantity;
+        this.bookId = bookId;
+    }
+
+    public void cartItems() {}
+
+
+    public String getCartItemId() {
+        return cartItemId;
+    }
+
+    public void setCartItemId(String cartItemId) {
+        this.cartItemId = cartItemId;
+    }
+
+    public String getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(String cartId) {
+        this.cartId = cartId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+}
