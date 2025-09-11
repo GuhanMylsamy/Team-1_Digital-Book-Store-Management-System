4898436e6ab3174b3c8dab39dfe893162e4741a8
Anand <2440589@cognizant.com>
2025-09-12 00:13:01 +0530

    Created Review Service and Review controller

--- src/main/java/com/libraryManagement/project/repository/ReviewRepository.java
+++ src/main/java/com/libraryManagement/project/repository/ReviewRepository.java
@@ -3,5 +3,9 @@ package com.libraryManagement.project.repository;
 import com.libraryManagement.project.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+List<Review> findByBookId(Long bookId);
 }
