e9c91af8e59fc2a88e294178fdc611dcb7144b88
Taufique <Taufiquemeraj.ansari@cognizant.com>
2025-09-16 09:27:39 +0530

    Added swagger

--- src/main/java/com/libraryManagement/project/config/SecurityConfig.java
+++ src/main/java/com/libraryManagement/project/config/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,13 +16,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF
-                .httpBasic(httpBasic -> httpBasic.disable()) // Disable HTTP Basic
-                .formLogin(formLogin -> formLogin.disable()) // Disable form login
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF
+                .httpBasic(AbstractHttpConfigurer::disable) // Disable HTTP Basic
+                .formLogin(AbstractHttpConfigurer::disable) // Disable form login
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll() // Allow all requests
                 );
 
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(); // No users defined
+    }
+
 }
--- src/main/java/com/libraryManagement/project/exception/GlobalExceptionHandler.java
+++ src/main/java/com/libraryManagement/project/exception/GlobalExceptionHandler.java
@@ -32,28 +32,5 @@ public class GlobalExceptionHandler {
                 .body(Map.of("status", "NOT_FOUND", "message", ex.getMessage()));
     }
 
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<?> handleUserNotFound(UserNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(Map.of("status", "NOT_FOUND", "message", ex.getMessage()));
-    }
-
-    @ExceptionHandler(DeletionException.class)
-    public ResponseEntity<?> handleDeletionError(DeletionException e){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Map.of("status","BAD_REQUEST","message",e.getMessage()));
-    }
-
-    @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<?> handleOtherErrors(UnauthorizedException ex) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                .body(Map.of("status", "FORBIDDEN", "message", ex.getMessage()));
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleOtherErrors(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Map.of("status", "ERROR", "message", ex.getMessage()));
-    }
 }
 
