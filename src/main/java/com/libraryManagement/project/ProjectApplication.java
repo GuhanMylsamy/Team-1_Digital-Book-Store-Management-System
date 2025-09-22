dbc5c0dec6e610ea3d51ebeebb73b40957ee0e23
Yadav <2440540@cognizant.com>
2025-09-22 17:09:25 +0530

    Fixed bugs

--- src/main/java/com/libraryManagement/project/ProjectApplication.java
+++ src/main/java/com/libraryManagement/project/ProjectApplication.java
@@ -1,7 +1,6 @@
 package com.libraryManagement.project;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/libraryManagement/project/controller/CartController.java
+++ src/main/java/com/libraryManagement/project/controller/CartController.java
@@ -13,11 +13,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/cart")
 public class CartController {
-<<<<<<< HEAD
-=======
-
-
->>>>>>> main
     private final CartService cartService;
 
     @Autowired
