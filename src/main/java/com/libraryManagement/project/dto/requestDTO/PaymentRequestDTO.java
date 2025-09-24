2c66e1b3e868d9caa0ea0446bd6e68ae409d5f57
Yadav <2440540@cognizant.com>
2025-09-24 10:29:42 +0530

    Unit tests added for payment functionality

--- src/main/java/com/libraryManagement/project/dto/requestDTO/PaymentRequestDTO.java
+++ src/main/java/com/libraryManagement/project/dto/requestDTO/PaymentRequestDTO.java
@@ -1,8 +1,12 @@
 package com.libraryManagement.project.dto.requestDTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentRequestDTO {
     private String type;
     private String identifier;
